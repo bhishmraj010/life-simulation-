160bdf66cb2db13a1e098e1203013f18d83236e8
bhishmraj <yogansh37@gmail.com>
2026-06-04 12:53:46 +0530

    Update settings.py

--- life_simulation/settings.py
+++ life_simulation/settings.py
@@ -55,11 +55,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'life_simulation.wsgi.application'
 
-# Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': os.environ.get('PGHOST'),
+        'PORT': os.environ.get('PGPORT', '5432'),
     }
 }
 
