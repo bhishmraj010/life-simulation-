ad59393f042c44eec19a61ea7e9e7d7b6906c3e3
bhishmraj010 <yogansh37@gmail.com>
2026-07-23 20:47:18 +0530

    Switch to PostgreSQL

--- life_simulation/life_simulation/settings.py
+++ life_simulation/life_simulation/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -94,10 +95,11 @@ WSGI_APPLICATION = "life_simulation.wsgi.application"
 # ==========================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # ==========================
